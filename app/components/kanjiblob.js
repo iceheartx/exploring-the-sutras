6dd354f36517217d74328a4b1523dcb6ca029f38
iceheartx <github@iceheart.org>
2016-02-15 00:50:01 -0800

    added character num [fixes #113722389]

--- app/components/kanjiblob.js
+++ app/components/kanjiblob.js
@@ -4,16 +4,15 @@ const cx = require('classnames');
 
 class kanjiblob extends React.Component {
     render() {
-        const {kanji,hiragana,romaji,english,title} = this.props.kanjiBlob;
+        const {kanji,hiragana,romaji,english,title, key} = this.props.kanjiBlob;
         const classNames = cx('blob', (title === 'true') ? 'title col-xs-4' : 'col-xs-3');
         return (
             <div className={classNames}>
                 <div className={cx('kanji', (this.props.store.displayKanji !== true) ? 'hidden' : '') }>{kanji}</div>
                 <div className={cx('romaji', (this.props.store.displayRomaji !== true) ? 'hidden' : '') }>{romaji}</div>
-                <div
-                    className={cx('english', (this.props.store.displayEnglish !== true) ? 'hidden' : '') }>{english}</div>
-                <div
-                    className={cx('hiragana', (this.props.store.displayHiragana !== true) ? 'hidden' : '') }>{hiragana}</div>
+                <div className={cx('english', (this.props.store.displayEnglish !== true) ? 'hidden' : '') }>{english}</div>
+                <div className={cx('hiragana', (this.props.store.displayHiragana !== true) ? 'hidden' : '') }>{hiragana}</div>
+                <div className={cx('characterNum', (this.props.store.displayCharacterNum !== true) ? 'hidden' : '') }>{key}</div>
             </div>
         );
     }
